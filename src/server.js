a72f3c1f02f02b407245f43175fdf63332eb269f
Liam Ronan <liamronan16@gmail.com>
2024-10-25 19:20:37 +0100

    ci: added commitlint to the CI

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+console.log('hello world');
 app.get('/', (req, res) => {
   res.json({ message: 'hello world' });
 });
